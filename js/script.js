496619072d950141a620a74e97406feb59092cb2
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2019-06-07 15:13:47 +0300

    Поработал над улучшением формы обратной связи.
    - Использовал `localStorage` для хранения имени и email пользователя.
    - Добавил анимацию при попытке отправки пустой формы.

--- js/script.js
+++ js/script.js
@@ -5,15 +5,60 @@ var modalContactsClose = modalContacts.querySelector('.modal__close');
 
 var overlay = document.querySelector('.overlay');
 
+var contactsForm = modalContacts.querySelector('.contacts-form');
+var userData = {
+  name: document.getElementById('contacts-name'),
+  email: document.getElementById('contacts-email'),
+  message: document.getElementById('contacts-message')
+};
+
+var isStorageSupport = true;
+var storage = {
+  name: '',
+  email: ''
+};
+
+// Проверка, работает ли localStorage
+try {
+  storage.name = localStorage.getItem('name');
+  storage.email = localStorage.getItem('email');
+} catch (err) {
+  isStorageSupport = false;
+}
+
 // Оживление всплывающего окна формы обратной связи
 contactsLink.addEventListener('click', function (evt) {
   evt.preventDefault();
   modalShow(modalContacts);
+
+  if (storage.name && storage.email) {
+    userData.name.value = storage.name;
+    userData.email.value = storage.email;
+    userData.message.focus();
+  } else {
+    userData.name.focus();
+  }
 });
 
 modalContactsClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   modalClose(modalContacts);
+  modalContacts.classList.remove('modal--error');
+});
+
+// Сохранеие данных пользователя в localStorage
+contactsForm.addEventListener('submit', function (evt) {
+  if (!userData.name.value || !userData.email.value || !userData.message.value) {
+    evt.preventDefault();
+    modalContacts.classList.remove('modal--error');
+    modalContacts.offsetWidth = modalContacts.offsetWidth;
+    modalContacts.classList.add('modal--error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('name', userData.name.value);
+      localStorage.setItem('email', userData.email.value);
+    }
+  }
 });
 
 // Оживление всплывающего окна интерактивной карты
@@ -38,6 +83,7 @@ window.addEventListener('keydown', function (evt) {
     evt.preventDefault();
     if (modalContacts.classList.contains('modal--shown')) {
       modalClose(modalContacts);
+      modalContacts.classList.remove('modal--error');
     }
     if (modalMap.classList.contains('modal--shown')) {
       modalClose(modalMap);
@@ -49,6 +95,7 @@ window.addEventListener('keydown', function (evt) {
 overlay.addEventListener('click', function () {
   if (modalContacts.classList.contains('modal--shown')) {
     modalClose(modalContacts);
+    modalContacts.classList.remove('modal--error');
   }
   if (modalMap.classList.contains('modal--shown')) {
     modalClose(modalMap);
