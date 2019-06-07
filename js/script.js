bea39bb0711653fc911ddf553cb9921e313c0832
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2019-06-07 12:47:15 +0300

    :sparkles: Сделал появление и закрытие попапа интерактивной карты на JS.

--- js/script.js
+++ js/script.js
@@ -33,3 +33,37 @@ overlay.addEventListener('click', function () {
     overlay.classList.remove('overlay--shown');
   }
 });
+
+var mapLink = document.querySelector('.contacts__map-image');
+
+var modalMap = document.querySelector('.modal--map');
+var modalMapClose = modalMap.querySelector('.modal__close');
+
+mapLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalMap.classList.add('modal--shown');
+  overlay.classList.add('overlay--shown');
+});
+
+modalMapClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalMap.classList.remove('modal--shown');
+  overlay.classList.remove('overlay--shown');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalMap.classList.contains('modal--shown')) {
+      modalMap.classList.remove('modal--shown');
+      overlay.classList.remove('overlay--shown');
+    }
+  }
+});
+
+overlay.addEventListener('click', function () {
+  if (modalMap.classList.contains('modal--shown')) {
+    modalMap.classList.remove('modal--shown');
+    overlay.classList.remove('overlay--shown');
+  }
+});
