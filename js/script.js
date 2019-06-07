bf17fbdda008c120a6500de1c6720ef1806e9d6a
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2019-06-07 17:23:33 +0300

    Фикс: исправил ошибку, из-за которой появление попапа не срабатывало на странице каталога.

--- js/script.js
+++ js/script.js
@@ -1,143 +1,133 @@
 'use strict';
 
-var overlay = document.querySelector('.overlay');
-
+// Оживление всплывающего окна формы обратной связи
 var contactsLink = document.querySelector('.contacts__button');
 
 var modalContacts = document.querySelector('.modal--contacts');
-var modalContactsClose = modalContacts.querySelector('.modal__close');
 
-var contactsForm = modalContacts.querySelector('.contacts-form');
-var userData = {
-  name: document.getElementById('contacts-name'),
-  email: document.getElementById('contacts-email'),
-  message: document.getElementById('contacts-message')
-};
+if (modalContacts) {
+  var modalContactsClose = modalContacts.querySelector('.modal__close');
 
-var isStorageSupport = true;
-var storage = {
-  name: '',
-  email: ''
-};
+  var contactsForm = modalContacts.querySelector('.contacts-form');
+  var userData = {
+    name: document.getElementById('contacts-name'),
+    email: document.getElementById('contacts-email'),
+    message: document.getElementById('contacts-message')
+  };
 
-// Проверка, работает ли localStorage
-try {
-  storage.name = localStorage.getItem('name');
-  storage.email = localStorage.getItem('email');
-} catch (err) {
-  isStorageSupport = false;
-}
+  var isStorageSupport = true;
+  var storage = {
+    name: '',
+    email: ''
+  };
 
-// Оживление всплывающего окна формы обратной связи
-contactsLink.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modalShow(modalContacts);
-
-  if (storage.name && storage.email) {
-    userData.name.value = storage.name;
-    userData.email.value = storage.email;
-    userData.message.focus();
-  } else {
-    userData.name.focus();
+  // Проверка, работает ли localStorage
+  try {
+    storage.name = localStorage.getItem('name');
+    storage.email = localStorage.getItem('email');
+  } catch (err) {
+    isStorageSupport = false;
   }
-});
 
-modalContactsClose.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modalClose(modalContacts);
-  modalContacts.classList.remove('modal--error');
-});
-
-// Сохранеие данных пользователя в localStorage
-contactsForm.addEventListener('submit', function (evt) {
-  if (!userData.name.value || !userData.email.value || !userData.message.value) {
+  // Появление всплывающего окна
+  contactsLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    modalContacts.classList.remove('modal--error');
-    void modalContacts.offsetWidth;
-    modalContacts.classList.add('modal--error');
+    modalShow(modalContacts);
 
-    if (!userData.name.value) {
-      userData.name.focus();
-    } else if (!userData.email.value) {
-      userData.email.focus();
-    } else {
+    if (storage.name && storage.email) {
+      userData.name.value = storage.name;
+      userData.email.value = storage.email;
       userData.message.focus();
+    } else {
+      userData.name.focus();
     }
-  } else {
-    if (isStorageSupport) {
+  });
+
+  // Закрытие всплывающего окна
+  modalContactsClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalClose();
+  });
+
+  // Сохранеие данных пользователя в localStorage
+  contactsForm.addEventListener('submit', function (evt) {
+    if (!userData.name.value || !userData.email.value || !userData.message.value) {
+      evt.preventDefault();
+      modalContacts.classList.remove('modal--error');
+      void modalContacts.offsetWidth;
+      modalContacts.classList.add('modal--error');
+
+      if (!userData.name.value) {
+        userData.name.focus();
+      } else if (!userData.email.value) {
+        userData.email.focus();
+      } else {
+        userData.message.focus();
+      }
+    } else if (isStorageSupport) {
       localStorage.setItem('name', userData.name.value);
       localStorage.setItem('email', userData.email.value);
     }
-  }
-});
+  });
+}
 
 // Оживление всплывающего окна интерактивной карты
 var mapLink = document.querySelector('.contacts__map-image');
 
 var modalMap = document.querySelector('.modal--map');
-var modalMapClose = modalMap.querySelector('.modal__close');
 
-mapLink.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modalShow(modalMap);
-});
+if (modalMap) {
+  var modalMapClose = modalMap.querySelector('.modal__close');
 
-modalMapClose.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modalClose(modalMap);
-});
+  mapLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalShow(modalMap);
+  });
+
+  modalMapClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalClose();
+  });
+}
 
 // Оживление всплывающего окна добавления товара в корзину
 var cartButtons = document.querySelectorAll('.button--buy');
 
 var modalCart = document.querySelector('.modal--cart');
-var modalCartClose = modalCart.querySelector('.modal__close');
 
-var addClickHandler = function (buyButton) {
-  buyButton.addEventListener('click', function (evt) {
+if (modalCart) {
+  var modalCartClose = modalCart.querySelector('.modal__close');
+
+  var addClickHandler = function (buyButton) {
+    buyButton.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      modalShow(modalCart);
+    });
+  };
+
+  for (var i = 0; i < cartButtons.length; i++) {
+    addClickHandler(cartButtons[i]);
+  }
+
+  modalCartClose.addEventListener('click', function (evt) {
     evt.preventDefault();
-    modalShow(modalCart);
+    modalClose();
   });
-};
-
-for (var i = 0; i < cartButtons.length; i++) {
-  addClickHandler(cartButtons[i]);
 }
-
-modalCartClose.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  modalClose(modalCart);
-});
 
 // Закрытие всплывающего окна по нажатию Esc
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (modalContacts.classList.contains('modal--shown')) {
-      modalClose(modalContacts);
-      modalContacts.classList.remove('modal--error');
-    }
-    if (modalMap.classList.contains('modal--shown')) {
-      modalClose(modalMap);
-    }
-    if (modalCart.classList.contains('modal--shown')) {
-      modalClose(modalCart);
-    }
+    modalClose();
   }
 });
 
 // Закрытие всплывающего окна по клику вне окна
+var overlay = document.querySelector('.overlay');
+
 overlay.addEventListener('click', function () {
-  if (modalContacts.classList.contains('modal--shown')) {
-    modalClose(modalContacts);
-    modalContacts.classList.remove('modal--error');
-  }
-  if (modalMap.classList.contains('modal--shown')) {
-    modalClose(modalMap);
-  }
-  if (modalCart.classList.contains('modal--shown')) {
-    modalClose(modalCart);
-  }
+  modalClose();
 });
 
 // Появление всплывающего окна
@@ -147,7 +137,15 @@ function modalShow(elmt) {
 }
 
 // Закрытие всплывающего окна
-function modalClose(elmt) {
-  elmt.classList.remove('modal--shown');
-  overlay.classList.remove('overlay--shown');
+function modalClose() {
+  var modalShown = document.querySelector('.modal--shown');
+
+  if (modalShown) {
+    modalShown.classList.remove('modal--shown');
+    overlay.classList.remove('overlay--shown');
+
+    if (modalShown.classList.contains('modal--error')) {
+      modalShown.classList.remove('modal--error');
+    }
+  }
 }
